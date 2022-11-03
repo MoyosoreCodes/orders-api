acd64798bebbf2a5e267e5be3e396286132ef5e5
Moyosore <31387340+MoyosoreCodes@users.noreply.github.com>
2022-11-03 22:29:47 +0100

    update: added api link to app page

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return `Order API is now running, view it <a href='https://documenter.getpostman.com/view/12993294/2s8YY9xSqR'> here </a>`;
   }
 }
